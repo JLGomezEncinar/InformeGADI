988ad38d34d322f2047ccf11eb0acca8aa779e84
José Luis Gómez <240092@iessanalberto.com>
2026-01-16 10:52:14 +0100

    Update

--- app/index.js
+++ app/index.js
@@ -19,24 +19,32 @@ export default function Index() {
         { label: '2018', value: '2018' },
         { label: '2019', value: '2019' },
     ];
+    
+
 
     const [value, setValue] = useState("Todos")
     const [datos, setDatos] = useState([]);
-    const [nombre, setNombre] = useState('') 
-    const [isChecked, setChecked] = useState(false)
+    const [nombre, setNombre] = useState('');
+    const [isChecked, setChecked] = useState(false);
+    const [nominacion, setNominacion] = useState(1);
 
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/JLGomezEncinar/FicheroJSON/refs/heads/main/games.json')
             .then(res => res.json())
             .then(data => setDatos(data));
     }, []);
-
-    const datosFiltrados = datos.filter( d => {
+    
+   
+  
+    const datosFiltrados = datos.filter(d => {
         const cumpleYear = value === "Todos"
-        || d.year == value;
+            || d.year == value;
         const cumpleGanador = !isChecked || isChecked == (d.winner == 1);
         const cumpleNombre = nombre === '' || d.nominee.toLowerCase().includes(nombre.toLowerCase());
-        return (cumpleYear && cumpleGanador && cumpleNombre)});
+        
+        return (cumpleYear && cumpleGanador && cumpleNombre)
+    });
+    
 
 
     return (
@@ -62,8 +70,10 @@ export default function Index() {
                                 setChecked={setChecked}
                                 nombre={nombre}
                                 setNombre={setNombre}
+                                nominacion={nominacion}
+                                setNominacion={setNominacion}
                             />
-                             <View style={[styles.row, styles.header]}>
+                            <View style={[styles.row, styles.header]}>
                                 <Text style={styles.headerCell}>Año</Text>
                                 <Text style={styles.headerCell}>Categoría</Text>
                                 <Text style={styles.headerCell}>Nombre</Text>
